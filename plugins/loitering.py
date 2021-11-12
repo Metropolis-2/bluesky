4e07973b9013f7fc98a5dfb8e9c487f5a46856b7
andubadea <37139260+andubadea@users.noreply.github.com>
2021-11-12 13:58:33 +0100

    Loitering can be accessed through bs.traf.loiter.

--- plugins/loitering.py
+++ plugins/loitering.py
@@ -26,9 +26,11 @@ class Loitering(Entity):
         with self.settrafarrays():
             self.futuregeofences = []
             self.geodurations = []
-        
+        bs.traf.loiter = self
+    
+    @staticmethod
     @stack.command
-    def creloiter(self, acid, actype, aclat, aclon, achdg, acalt, acspd, geodur, *geocoords):
+    def creloiter(acid, actype, aclat, aclon, achdg, acalt, acspd, geodur, *geocoords):
         '''Create a loitering aircraft.'''
         # First, create aircraft
         bs.traf.cre(acid, actype, aclat, aclon, achdg, acalt, acspd)
@@ -37,33 +39,34 @@ class Loitering(Entity):
         acidx = bs.traf.id.index(acid)
         
         # Store the geofence data in the array until it needs to be enacted
-        self.futuregeofences[acidx] = geocoords
-        self.geodurations[acidx] = geodur
+        bs.traf.loiter.futuregeofences[acidx] = geocoords
+        bs.traf.loiter.geodurations[acidx] = geodur
     
+    @staticmethod
     @stack.command
-    def delloiter(self, acidx:'acid'):
+    def delloiter(acidx:'acid'):
         '''Delete loitering aircraft, add its geofence.'''
         acid = bs.traf.id[acidx]
         # First of all, add the geofence 
-        self.loitergeofences[acid] = {'geofence':Geofence(f'LOITER{acid}', self.futuregeofences[acidx]), 
-                                      'time_left':self.geodurations[acidx]}
+        bs.traf.loiter.loitergeofences[acid] = {'geofence':Geofence(f'LOITER{acid}', bs.traf.loiter.futuregeofences[acidx]), 
+                                      'time_left':bs.traf.loiter.geodurations[acidx]}
         # Then delete the aircraft
         bs.traf.delete(acidx)
         
-        
+    @staticmethod   
     @timed_function(dt = check_dt)
-    def keep_track_loitering(self):
+    def keep_track_loitering():
         '''Keep track of loiter geofences, and delete them when they have expired.'''
         # iterate through dictionary entries
         # This shouldn't take too long, there won't be many entries in this dictionary
-        for acid in self.loitergeofences:
+        for acid in bs.traf.loiter.loitergeofences:
             # Decrement time
-            self.loitergeofences[acid]['time_left'] -= check_dt
+            bs.traf.loiter.loitergeofences[acid]['time_left'] -= check_dt
             # Check if time is negative
-            if self.loitergeofences[acid]['time_left'] < 0:
+            if bs.traf.loiter.loitergeofences[acid]['time_left'] < 0:
                 # Delete geofence
                 Geofence.delete(f'LOITER{acid}')
-                self.loitergeofences.pop(acid)
+                bs.traf.loiter.loitergeofences.pop(acid)
             
             
             
