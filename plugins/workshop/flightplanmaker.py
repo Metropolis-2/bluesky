cecba1d028b52fa9af063e6aa7c9cb185100b25a
andubadea <37139260+andubadea@users.noreply.github.com>
2022-07-12 11:29:57 +0200

    bugfixes

--- plugins/workshop/flightplanmaker.py
+++ plugins/workshop/flightplanmaker.py
@@ -21,13 +21,10 @@ class FlightPlanMaker(Entity):
     def create(self, n=1):
         ''' Create is called when new aircraft are created. '''
         super().create(n)
-        self.c2c_fp_loaded[-n:] = False
 
     def generate_c2c_fp_from_WP(self, acidx, filename):
-        message_dict = {}
-        message_dict["conflicting_id"] = "gca-paparazzi-1"
-        message_dict["timestamp"] =  datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z" # current date and time in UTC.strftime("%Y-%m-%dT%H:%M:%S.$f") # current date and time in UTC
-        #message_dict["timestamp"] = "2022-02-16T17:02:17.000Z"
+        if '.json' not in filename:
+            filename += '.json'
         flightplan_dict = {}
         flightplan_dict["version"] = "1.1.0"
         flightplan_dict["FlightPoints"] = []
@@ -39,11 +36,10 @@ class FlightPlanMaker(Entity):
             flightpoint["AltitudeAMSL"] = bs.traf.ap.route[acidx].wpalt[i]
             flightplan_dict["FlightPoints"].append(flightpoint)
             
-        message_dict["flightplan"] = json.dumps(flightplan_dict) + '\n'
-        with open(filename, 'w') as f:
+        with open('data/flightplans/' + filename, 'w') as f:
             # Save with spacing and tabs
             json.dump(flightplan_dict, f, indent=6)
-        return message_dict
+        return True
 
     @stack.command()
     def MAKEFLIGHTPLAN(self, acid: 'acid', fp_file: str):
