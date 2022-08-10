f3eea83fa03410ce7d0a2bd3972224e047a84a2d
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-08-10 09:57:01 +0200

    update cmd split

--- scenario_translator.py
+++ scenario_translator.py
@@ -4,6 +4,7 @@ from os import path
 import geopandas as gpd
 from shapely.affinity import rotate, scale, translate
 from pyproj import Transformer
+from typing import Union
 import warnings
 from shapely.errors import ShapelyDeprecationWarning
 warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning)
@@ -21,11 +22,10 @@ xy_offset_m = (-450, -660)
 transformer_m = Transformer.from_crs(4326, 3857, always_xy=True)
 transformer_deg = Transformer.from_crs(3857, 4326)
 
-# now open the scenario data
-with open(scenario, 'r') as f:
-    data = f.readlines()
-
 def translate_data(lat: str,lon: str):
+    '''
+    Translate the lat and lon data with a prededined offset
+    '''
     # Step 1
     # transform to UTM
     x, y = transformer_m.transform(float(lon), float(lat))
@@ -39,15 +39,65 @@ def translate_data(lat: str,lon: str):
     
     return str(lat_t), str(lon_t)
 
+def cmd_translator(cmd_split: Union[list, None] = None):
+    ''' CRE, ADDWAYPOINTS, ATDIST translator
+
+    The CRE command is as follows: xs
+        hh:mm:ss>CRE [acid] [type] [lat] [lon] [hdg] [spd] [alt]
+    
+    The ADDWAYPOINTS works as follows
+        hh:mm:ss>ADDWAYPOINTS [acid] [lat1] [lon1] [alt1] [spd1] [wptype1] [turnspd1] [lat2] [lon2] [alt2] [spd2] [wptype2] [turnspd2] [lat3] ...
+    
+    The ATDIST Command works as follows
+        hh:mm:ss>ATDIST [acid] [lat] [lon] [dist] [cmd]
+    '''
+
+    if cmd_split[0] not in ['CRE', 'ADDWAYPOINTS', 'ATDIST']:
+        return cmd_split
+
+    if cmd_split[0] == 'CRE':
+        # if CRE CMD lat,lon are 3 and 4 positions
+        lat_t, lon_t =  translate_data(*cmd_split[3:5])
+        cmd_split[3:5] = [lat_t, lon_t]
+        new_cmd = ' '.join(cmd_split)
+
+    elif cmd_split[0] == 'ATDIST':
+        # if ATDIST CMD lat,lon are 2 and 3 positions
+        lat_t, lon_t =  translate_data(*cmd_split[2:4])
+        cmd_split[2:4] = [lat_t, lon_t]
+
+        # call itself  to translate the command
+        cmd_split[5:] = cmd_translator(cmd_split[5:])
+
+        new_cmd = ' '.join(cmd_split)
+        
+    elif cmd_split[0] == 'ADDWAYPOINTS':
+        # ADDWAYPOINTS is more complicated
+        # lat is at postion 1 and then it repeats every 6 positions
+        # lon is at position 2 and then it repeats every 6 positions
+        # get lats and lons from ADDWAYPOINTS
+        lats = cmd_split[2::6]
+        lons = cmd_split[3::6]
+        
+        lat_lons = [translate_data(*lat_lon) for lat_lon in zip(lats, lons)]
+        lats_t = [lat_lon[0] for lat_lon in lat_lons]
+        lons_t = [lat_lon[1] for lat_lon in lat_lons]
+
+        cmd_split[2::6] = lats_t
+        cmd_split[3::6] = lons_t
+        
+        new_cmd = ' '.join(cmd_split)
+
+    return new_cmd
+
+
+# now open the scenario data
+with open(scenario, 'r') as f:
+    data = f.readlines()
+
 # now loop over the lines and translate the aircraft
 # only modify lines that start with:
 # CRE, ADDWAYPOINTS, ATDIST
-# The CRE command is as follows: modify just lat lon
-# hh:mm:ss>CRE [acid] [type] [lat] [lon] [hdg] [spd] [alt]
-# The ADDWAYPOINTS works as follows
-# hh:mm:ss>ADDWAYPOINTS [acid] [lat1] [lon1] [alt1] [spd1] [wptype1] [turnspd1] [lat2] [lon2] [alt2] [spd2] [wptype2] [turnspd2] [lat3] ...
-# The ATDIST Command works as follows
-# hh:mm:ss>ATDIST [acid] [lat] [lon] [dist] [cmd]
 
 line_new = []
 for idx, line in enumerate(data):
@@ -57,37 +107,9 @@ for idx, line in enumerate(data):
 
     if cmd_split[0] not in ['CRE', 'ADDWAYPOINTS', 'ATDIST']:
         line_new.append(line)
-
     else:
-        if cmd_split[0] == 'CRE':
-            # if CRE CMD lat,lon are 3 and 4 positions
-            lat_t, lon_t =  translate_data(*cmd_split[3:5])
-            cmd_split[3:5] = [lat_t, lon_t]
-            new_cmd = ' '.join(cmd_split)
-
-        elif cmd_split[0] == 'ATDIST':
-            # if ATDIST CMD lat,lon are 2 and 3 positions
-            lat_t, lon_t =  translate_data(*cmd_split[2:4])
-            cmd_split[2:4] = [lat_t, lon_t]
-            new_cmd = ' '.join(cmd_split)
-            
-        elif cmd_split[0] == 'ADDWAYPOINTS':
-            # ADDWAYPOINTS is more complicated
-            # lat is at postion 1 and then it repeats every 6 positions
-            # lon is at position 2 and then it repeats every 6 positions
-            # get lats and lons from ADDWAYPOINTS
-            lats = cmd_split[2::6]
-            lons = cmd_split[3::6]
-            
-            lat_lons = [translate_data(*lat_lon) for lat_lon in zip(lats, lons)]
-            lats_t = [lat_lon[0] for lat_lon in lat_lons]
-            lons_t = [lat_lon[1] for lat_lon in lat_lons]
-
-            cmd_split[2::6] = lats_t
-            cmd_split[3::6] = lons_t
-            
-            new_cmd = ' '.join(cmd_split)
-
+        # translate the new command
+        new_cmd = cmd_translator(cmd_split)
         # now join the new command
         line_new.append(time + new_cmd)
 
